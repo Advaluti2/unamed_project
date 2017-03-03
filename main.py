2f2fe8e2023474abe45ca61fbead579133c67e94
Michael <advaluti@gmail.com>
2017-03-03 17:36:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 #Untitled Project
+import random
 
 dirt = 0
+grass = 0
 
 while True:
     intake = input("> ")
     if intake == "":
-        dirt += 1
-        print("Dirt:", dirt)
+        x = random.randint(1, 101)
+        if x <= 50:
+            dirt += 1
+            print("Dirt:", dirt)
+        else:
+            grass += 1
+            print("Grass:", grass)
